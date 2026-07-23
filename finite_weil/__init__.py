e5382350bb23afd90c9cd773115ae2a09d395036
etocore <cote.rhys@gmail.com>
2026-07-23 17:16:20 -0400

    Export arithmetic and prime-operator APIs

--- finite_weil/__init__.py
+++ finite_weil/__init__.py
@@ -1,10 +1,36 @@
 """Finite-dimensional Weil operator tools."""
 
-from .packets import GaussianPacketFamily
+from .explicit_formula import (
+    PrimeOperatorTerm,
+    assemble_prime_operator,
+    prime_operator_terms,
+    quadratic_prime_power_coefficient,
+    universal_prime_operator,
+    von_mangoldt,
+)
+from .lfunctions import (
+    PrimitiveQuadraticCharacter,
+    is_fundamental_discriminant,
+    jacobi_symbol,
+    kronecker_symbol,
+    prime_power_base,
+)
 from .operators import generalized_eigenvalues, gram_operator_norm
+from .packets import GaussianPacketFamily
 
 __all__ = [
     "GaussianPacketFamily",
+    "PrimitiveQuadraticCharacter",
+    "PrimeOperatorTerm",
+    "assemble_prime_operator",
     "generalized_eigenvalues",
     "gram_operator_norm",
+    "is_fundamental_discriminant",
+    "jacobi_symbol",
+    "kronecker_symbol",
+    "prime_operator_terms",
+    "prime_power_base",
+    "quadratic_prime_power_coefficient",
+    "universal_prime_operator",
+    "von_mangoldt",
 ]
